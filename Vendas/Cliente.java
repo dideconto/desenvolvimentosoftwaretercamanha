6d26d73e111cd2dbff1f36a4404e07fd1cda44a7
aluno <Aluno@DOSO027251.educacional.up>
2022-04-05 12:02:24 -0300

    Projeto de Vendas - Cadastro do Cliente

---
+++ Vendas/Cliente.java
@@ -0,0 +1,24 @@
+public class Cliente {
+
+    private String nome;
+    private String cpf;
+    
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    @Override
+    public String toString() {
+        return "\nNome: " + nome + " | CPF: " + cpf;
+    }
+    
+}
--- Vendas/Principal.java
+++ Vendas/Principal.java
@@ -1,6 +1,11 @@
+import java.util.ArrayList;
+
 public class Principal {
     public static void main(String[] args) {
         int opcao = 0;
+        Cliente cliente = new Cliente();
+        ArrayList<Cliente> clientes = new ArrayList<Cliente>();
+
         do {
             System.out.println("\n -- PROJETO DE VENDAS -- \n");
             System.out.println("1 - Cadastrar Cliente");
@@ -11,6 +16,10 @@ public class Principal {
             switch (opcao) {
                 case 1:
                     System.out.println("\n -- CADASTRO DE CLIENTES -- \n");
+                    cliente.setNome(Console.readString("Digite o nome do cliente: "));
+                    cliente.setCpf(Console.readString("Digite o CPF do cliente: "));
+                    clientes.add(cliente);
+                    System.out.println("\nCliente cadastrado !!!");                    
                     break;
                 case 2:
                     System.out.println("\n -- LISTAGEM DE CLIENTES -- \n");
