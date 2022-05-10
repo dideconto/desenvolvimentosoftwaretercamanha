29e4ffccc64e13930d21395dbf0af8b22ad14d86
geucimar <geucimar@gmail.com>
2022-05-10 09:34:40 -0300

    Correção da Prova - Cálculos com métodos

--- Folha de Pagamento/views/Principal.java
+++ Folha de Pagamento/views/Principal.java
@@ -1,47 +1,67 @@
 package views;
 
-public class Principal{
+public class Principal {
     public static void main(String[] args) {
-        int horas = 100; 
-        int valor = 20;
+        int horas = 100;
+        int valor = 50;
         double bruto, irrf, inss, fgts, liquido;
 
-        //Calcular o salário bruto
-        bruto = horas * valor;
+        // Calcular o salário bruto
+        bruto = calcularSalarioBruto(horas, valor);
         System.out.println("Salário bruto: " + bruto);
 
-        //Calcular o imposto de renda
-        if(bruto <= 1903.98){
-            irrf = 0;
-        }else if(bruto <= 2826.65){
-            irrf = (bruto * .075) - 142.8;
-        }else if(bruto <= 3751.05){
-            irrf = (bruto * .15) - 354.8;
-        }else if(bruto <= 4664.68){
-            irrf = (bruto * .225) - 636.13;
-        }else{
-            irrf = (bruto * .275) - 869.39;
-        }
+        // Calcular o imposto de renda
+        irrf = calcularIRRF(bruto);
         System.out.println("IRRF: " + irrf);
-        
-        //Calcular o INSS
-        if(bruto <= 1693.72){
-            inss = .08;
-        }else if(bruto <= 2822.9){
-            inss = bruto * .09;
-        }else if(bruto <= 5645.8){
-            inss = bruto * .11;
-        }else{
-            inss = 621.03;
-        }
+
+        // Calcular o INSS
+        inss = calcularINSS(bruto);
         System.out.println("INSS: " + inss);
-        
-        //Calcular o FGTS
-        fgts = bruto * .08;
+
+        // Calcular o FGTS
+        fgts = calcularFGTS(bruto);
         System.out.println("FGTS: " + fgts);
-        
-        //Calcular o FGTS
-        liquido = bruto - inss - irrf;
+
+        // Calcular o salário líquido
+        liquido = calcularSalarioLiquido(bruto, irrf, inss);
         System.out.println("Salário líquido: " + liquido);
+    }
+
+    public static double calcularSalarioBruto(int horas, int valor) {
+        return horas * valor;
+    }
+
+    public static double calcularIRRF(double bruto) {
+        if (bruto <= 1903.98) {
+            return 0;
+        } else if (bruto <= 2826.65) {
+            return (bruto * .075) - 142.8;
+        } else if (bruto <= 3751.05) {
+            return (bruto * .15) - 354.8;
+        } else if (bruto <= 4664.68) {
+            return (bruto * .225) - 636.13;
+        }
+        return (bruto * .275) - 869.39;
+
+    }
+
+    public static double calcularINSS(double bruto) {
+        if (bruto <= 1693.72) {
+            return bruto * .08;
+        } else if (bruto <= 2822.9) {
+            return bruto * .09;
+        } else if (bruto <= 5645.8) {
+            return bruto * .11;
+        }
+        return 621.03;
+    }
+
+    public static double calcularFGTS(double bruto) {
+        return bruto * .08;
+    }
+
+    public static double calcularSalarioLiquido(double bruto,
+            double irrf, double inss) {
+        return bruto - irrf - inss;
     }
 }
